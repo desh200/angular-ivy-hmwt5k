1d108ecd32d433cb6850f2e7dce2e2d29e3d9d3c
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 09:54:17 +0530

    1 file modified

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -4,10 +4,15 @@ import { Component, Input } from '@angular/core';
   selector: 'product',
   template: `
   <div class="media">
-  <img class="mr-3" src="..." alt="Generic placeholder image">
+  <img class="mr-3" src="data.imageUrl" alt="Generic placeholder image">
   <div class="media-body">
-    <h5 class="mt-0">Media heading</h5>
-    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    <h5 class="mt-0">{{data.ProductName}}</h5>
+    {{data.ProductRelease}}
+    <rating
+    [rating] ="data.rating" 
+    [numOfReviews]="data.numOfReviews"
+    ></rating><br>
+    {{data.description}}
   </div>
 </div>
   `,
