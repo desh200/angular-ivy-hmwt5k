9113a0966d2f9e521154f26bc4a3180958ac753f
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 08:15:33 +0530

    2 files modified

--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -4,7 +4,14 @@ import { Component } from '@angular/core';
   template: `
 <ng-container *ngIf="rating < 1; else elseBlock"><star></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
-
+<ng-container *ngIf="rating < 2; else elseBlock"><star></star></ng-container>
+<ng-template #elseBlock><starfill></starfill></ng-template>
+<ng-container *ngIf="rating < 3; else elseBlock"><star></star></ng-container>
+<ng-template #elseBlock><starfill></starfill></ng-template>
+<ng-container *ngIf="rating < 4; else elseBlock"><star></star></ng-container>
+<ng-template #elseBlock><starfill></starfill></ng-template>
+<ng-container *ngIf="rating < 5; else elseBlock"><star></star></ng-container>
+<ng-template #elseBlock><starfill></starfill></ng-template>
 `,
 })
 export class RatingComponent {
