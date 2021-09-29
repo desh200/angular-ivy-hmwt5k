97d8262f2e1a5a46ea54b5a2af5549de15432384
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 11:23:10 +0530

    1 file modified

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -4,7 +4,7 @@ import { ProductService } from './product.service';
 @Component({
   selector: 'products',
   template: `
-  <h2>Products></h2>
+  <h2>Products</h2>
   <div *ngFor="let product of products">
   <product [data] = "product"></product>
   </div>
