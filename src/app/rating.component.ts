62e208ec154896a8620bdf4705ebeaca5fe1221c
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 08:27:49 +0530

    1 file modified

--- src/app/rating.component.ts
+++ src/app/rating.component.ts
@@ -2,15 +2,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'rating',
   template: `
-<ng-container *ngIf="rating < 1; else elseBlock"><star></star></ng-container>
+<ng-container *ngIf="rating < 1; else elseBlock"><star (click)=onClick(1)></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
-<ng-container *ngIf="rating < 2; else elseBlock"><star></star></ng-container>
+<ng-container *ngIf="rating < 2; else elseBlock"><star (click)=onClick(2)></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
-<ng-container *ngIf="rating < 3; else elseBlock"><star></star></ng-container>
+<ng-container *ngIf="rating < 3; else elseBlock"><star (click)=onClick(3)></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
-<ng-container *ngIf="rating < 4; else elseBlock"><star></star></ng-container>
+<ng-container *ngIf="rating < 4; else elseBlock"><star (click)=onClick(4)></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
-<ng-container *ngIf="rating < 5; else elseBlock"><star></star></ng-container>
+<ng-container *ngIf="rating < 5; else elseBlock"><star (click)=onClick(5)></star></ng-container>
 <ng-template #elseBlock><starfill></starfill></ng-template>
 `,
 })
