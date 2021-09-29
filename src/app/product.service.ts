5a730175bb87104f2d5be2af316927e39902289e
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 10:53:22 +0530

    1 file modified

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,6 +9,30 @@ export class ProductService {
         rating: 4,
         numOfReviews: 2,
       },
+      {
+        imageUrl: 'https://loremflickr.com/150/150?random=2',
+        productName: 'product2',
+        ProductRelease: 'June 23,2017',
+        description: 'cras sit amet nibh libero, in gravita...',
+        rating: 2,
+        numOfReviews: 6,
+      },
+      {
+        imageUrl: 'https://loremflickr.com/150/150?random=3',
+        productName: 'product3',
+        ProductRelease: 'Mar 16,2016',
+        description: 'cras sit amet nibh libero, in gravita...',
+        rating: 3,
+        numOfReviews: 7,
+      },
+      {
+        imageUrl: 'https://loremflickr.com/150/150?random=4',
+        productName: 'product4',
+        ProductRelease: 'May 18,2016',
+        description: 'cras sit amet nibh libero, in gravita...',
+        rating: 5,
+        numOfReviews: 2,
+      },
     ];
   }
 }
