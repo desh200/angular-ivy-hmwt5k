ec40d14b6f5eefcdef3a9f4f35003c467a707306
desh200 <umesh.deshmukh35@gmail.com>
2021-09-29 11:00:49 +0530

    1 file added

---
+++ src/app/products.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+import { ProductService } from './product.service';
+
+@Component({
+  selector: 'products',
+  template: `
+  <h2>Products></h2>
+  <div *ngFor="let product of products">
+  <product [data] = "product"></product>
+  </div>
+  `,
+  providers: [ProductService],
+})
+export class ProductsComponent {
+  products;
+  constructor(productservice: ProductService) {
+    this.products = productservice.getProducts();
+  }
+}
