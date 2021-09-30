a6e8e678b0a409e39acde44a9159c687ff9e6608
desh200 <umesh.deshmukh35@gmail.com>
2021-09-30 09:25:40 +0530

    1 file modified

--- src/app/product.component.ts
+++ src/app/product.component.ts
@@ -2,7 +2,27 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'product',
-  template: ``,
+  template: `
+  <div class="media">
+  <img class="align-self-start mr-3" src="{{data.imageUrl}}" src="..." alt="Generic placeholder image">
+  <div class="media-body">
+    <h5 class="mt-0">{{data.ProductName}}</h5>
+    {{data.ProductRelease}}
+    <rating
+    [rating] ="data.rating" 
+    [numOfReviews]="data.numOfReviews"
+    ></rating><br>
+    {{data.description}}
+  </div>
+</div>
+  `,
+  styles: [
+    `.media
+  { 
+    margin-bottom:20px; 
+  }
+  `,
+  ],
 })
 export class ProductComponent {
   @Input() data;
