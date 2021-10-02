6f7a9f67ea0458b76285a11ae7cf08561d13853e
desh200 <umesh.deshmukh35@gmail.com>
2021-10-02 15:18:15 +0530

    2 files modified

--- src/app/Jumbotron.component.ts
+++ src/app/Jumbotron.component.ts
@@ -3,11 +3,11 @@ import { Component, Input } from '@angular/core';
   selector: 'bs-jumbotron',
   template: `
   <div class="jumbotron">
-  <h1 class="display-4">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <h1 class="display-4"><ng-content select=".heading"></ng-content></h1>
+  <p class="lead"><ng-content select=".body"></ng-content></p>
   <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  <p><ng-content select=".description"></ng-content></p>
+  <a class="btn btn-primary btn-lg" href="#" role="button"><ng-content select=".button"></ng-content></a>
 </div>
   `,
 })
